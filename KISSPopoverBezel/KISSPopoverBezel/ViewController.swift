953da3bc4df3671e77810f5f5196ecfe6ca0d8c8
DrabWeb <sethunity@gmail.com>
2016-01-24 11:29:06 -0400

    Added sound option, controlling both the fade in and fade out times, and full size image support

--- KISSPopoverBezel/KISSPopoverBezel/ViewController.swift
+++ KISSPopoverBezel/KISSPopoverBezel/ViewController.swift
@@ -22,14 +22,29 @@ class ViewController: NSViewController {
     // The image view for the bezel window
     @IBOutlet weak var imageView: NSImageView!
     
+    // The image view for the bezel window when it should take up the whole window
+    @IBOutlet weak var fullSizeImageView: NSImageView!
+    
     // The path to the image we want to show(If its not an OSX one)
     var imagePath : String = "";
+    
+    // The path to the sound file to play(If any)
+    var soundPath : String = "";
+    
+    // The actual sound
+    var sound : NSSound = NSSound();
     
     // How long the bezel window should stay on screen
     var screenTime : Float = 0;
     
     // Should we blend the label?
     var blendLabel : Bool = true;
+    
+    // How long the bezel window should take to fade out
+    var fadeOutTime : Float = 0;
+    
+    // Do we want to show a full size image?
+    var fullSize : Bool = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,16 +59,32 @@ class ViewController: NSViewController {
     
     func getAndSetValues() {
         // If the argument count is less than 6...
-        if(NSProcessInfo.processInfo().arguments.count < 7) {
+        if(NSProcessInfo.processInfo().arguments.count < 8) {
             // Print how to use the binary
-            print("Usage: KISSPopoverBezel <Label> <Image> <Fade in/out time> <How long to stay> <Blend label(true or false)> <Template(true or false)>");
+            print("Usage: KISSPopoverBezel <Label> <Image> <Fade in time> <Fade out time> <How long to stay> <Sound directory(Put \"none\" if you dont want one)> <Blend label(true or false)> <Template(true or false)>");
             
             // Quit the app
             quitApplication();
         }
         
-        // Set the label views text to the first argument
-        labelView.stringValue = NSProcessInfo.processInfo().arguments[1];
+        // If we passed a label...
+        if(NSProcessInfo.processInfo().arguments[1] != "") {
+            // Set the label views text to the first argument
+            labelView.stringValue = NSProcessInfo.processInfo().arguments[1];
+        }
+        else {
+            // Set fullSize to true
+            fullSize = true;
+            
+            // Hide the label
+            labelView.hidden = true;
+            
+            // Hide the regular image view
+            imageView.hidden = true;
+            
+            // Show the full size image view
+            fullSizeImageView.hidden = false;
+        }
         
         // If the second argument contains a /...
         if(NSProcessInfo.processInfo().arguments[2].containsString("/")) {
@@ -69,47 +100,92 @@ class ViewController: NSViewController {
                 imagePath = NSHomeDirectory() + imagePath;
             }
             
-            // Set the image views image to the image at the directory specified in the second argument
-            imageView.image = NSImage(byReferencingFile: imagePath);
+            // If we said to have a full size image...
+            if(fullSize) {
+                // Set the full size image views image to the image at the directory specified in the second argument
+                fullSizeImageView.image = NSImage(byReferencingFile: imagePath);
+            }
+            else {
+                // Set the image views image to the image at the directory specified in the second argument
+                imageView.image = NSImage(byReferencingFile: imagePath);
+            }
         }
         else {
-            // Set the image views image to the asset images specified in the second argument
-            imageView.image = NSImage(named: NSProcessInfo.processInfo().arguments[2]);
+            // If we said to have a full size image...
+            if(fullSize) {
+                // Set the full size image views image to the asset images specified in the second argument
+                fullSizeImageView.image = NSImage(named: NSProcessInfo.processInfo().arguments[2]);
+            }
+            else {
+                // Set the image views image to the asset images specified in the second argument
+                imageView.image = NSImage(named: NSProcessInfo.processInfo().arguments[2]);
+            }
         }
         
-        // If the iamge views image is nothing...
-        if(imageView.image == nil) {
-            // Print to the log thatit doesnt exist and prepose what they shoild try
-            print("Failed to load image \"" + NSProcessInfo.processInfo().arguments[2] + "\". Perhaps you mistyped it?");
+        // If we arent in full size mode...
+        if(!fullSize) {
+            // If the image views image is nothing...
+            if(imageView.image == nil) {
+                // Print to the log that it doesnt exist and prepose what they should try
+                print("Failed to load image \"" + NSProcessInfo.processInfo().arguments[2] + "\". Perhaps you mistyped it?");
+            }
+        }
+        else {
+            // If the full size image views image is nothing...
+            if(fullSizeImageView.image == nil) {
+                // Print to the log that it doesnt exist and prepose what they should try
+                print("Failed to load image \"" + NSProcessInfo.processInfo().arguments[2] + "\". Perhaps you mistyped it?");
+            }
         }
         
-        // Set the animators fade in / fade out time as the third argument(Converted to a float)
+        // Set the animators fade in time as the third argument(Converted to a float)
         NSAnimationContext.currentContext().duration = NSTimeInterval(NSString(string: NSProcessInfo.processInfo().arguments[3]).floatValue);
         
-        // Set the bezel windows screen time as the fourth argument
-        screenTime = NSString(string: NSProcessInfo.processInfo().arguments[4]).floatValue;
+        // Set the fade out time as the fourth argument(Converted to a float)
+        fadeOutTime = NSString(string: NSProcessInfo.processInfo().arguments[4]).floatValue;
         
-        // If the fifth argument is "true"...
-        if(NSProcessInfo.processInfo().arguments[5] == "true") {
+        // Set the bezel windows screen time as the fifth argument
+        screenTime = NSString(string: NSProcessInfo.processInfo().arguments[5]).floatValue;
+        
+        // If the sixth argument is not "none"...
+        if(NSProcessInfo.processInfo().arguments[6] != "none") {
+            // Set soundPath to the sixth argument
+            soundPath = NSProcessInfo.processInfo().arguments[6];
+            
+            // If the first character is a ~...
+            if(imagePath.characters.first == "~") {
+                // Remove the first character
+                soundPath = soundPath.substringFromIndex(soundPath.startIndex);
+                
+                // Prepend the home directory
+                soundPath = NSHomeDirectory() + soundPath;
+            }
+            
+            // Set sound to be the file at soundPath
+            sound = NSSound(contentsOfFile: soundPath, byReference: false)!;
+        }
+        
+        // If the seventh argument is "true"...
+        if(NSProcessInfo.processInfo().arguments[7] == "true") {
             // Set blendLabel to true
             blendLabel = true;
         }
-        // If the fifth argument is "false"...
-        else if(NSProcessInfo.processInfo().arguments[5] == "false") {
+        // If the seventh argument is "false"...
+        else if(NSProcessInfo.processInfo().arguments[7] == "false") {
             // Set blendLabel to false
             blendLabel = false;
         }
         
-        // If the sixth argument is true...
-        if(NSProcessInfo.processInfo().arguments[6] == "true") {
+        // If the eigth argument is true...
+        if(NSProcessInfo.processInfo().arguments[8] == "true") {
             // Set the image to be a template(Blend with vibrancy)
             imageView.image?.template = true;
         }
     }
     
     func fadeOutWindowAndQuit() {
-        // Set the animators fade in / fade out time as the third argument(Converted to a float)(For some reason it resets it before this happens)
-        NSAnimationContext.currentContext().duration = NSTimeInterval(NSString(string: NSProcessInfo.processInfo().arguments[3]).floatValue);
+        // Set the animators duration to the fade out time
+        NSAnimationContext.currentContext().duration = NSTimeInterval(fadeOutTime);
         
         // Fade out the window
         window.animator().alphaValue = 0;
@@ -169,6 +245,12 @@ class ViewController: NSViewController {
         
         // Fade in the window
         window.animator().alphaValue = 1;
+        
+        // If there was a sound loaded...
+        if(sound != NSSound()) {
+            // Play the sound
+            sound.play();
+        }
         
         // Start a timer for the on screen time + the animation duration, so it fades out and closes after the screen time is done
         NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(screenTime + Float(NSAnimationContext.currentContext().duration)), target:self, selector: Selector("fadeOutWindowAndQuit"), userInfo: nil, repeats:false);
--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Show a customized bezel window from the command line
 Download the source and open it in Xcode and hit run
 
 ## Usage
-```/path/to/KISSPopoverBezel.app/Contents/MacOS/KISSPopoverBezel <Label text> <Image path(Can also be the name of a OSX system image like NSComputer, NSColorPanel, ETC.)> <Fade in/out time> <How long to be on screen> <Should the label blend in? (true or false)> <Should the image be vibrant? (true or false)>```
+```/path/to/KISSPopoverBezel.app/Contents/MacOS/KISSPopoverBezel <Label> <Image> <Fade in time> <Fade out time> <How long to stay> <Sound directory(Put "none" if you dont want one)> <Blend label(true or false)> <Template(true or false)>```
 
 Example:
-```/path/to/KISSPopoverBezel.app/Contents/MacOS/KISSPopoverBezel NSLockUnlockedTemplate 0.2 1.5 false false```
+```/path/to/KISSPopoverBezel.app/Contents/MacOS/KISSPopoverBezel "KISSBezelWindow" NSLockUnlockedTemplate 0.1 0.3 1.5 /System/Library/Sounds/Pop.aiff false true```
 
 Also for easy access, I would recommend an alias
 ```alias kissbezelwindow="/path/to/KISSPopoverBezel.app/Contents/MacOS/KISSPopoverBezel"```
